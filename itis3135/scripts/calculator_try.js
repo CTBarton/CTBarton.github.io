ddc9cd7b9afee2a47123e30aa7f3a77878a3eb0f
MintChocoTaco <156540599+CTBarton@users.noreply.github.com>
2024-03-05 21:33:14 -0500

    Removing imbedded Java Script from .html

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -29,5 +29,35 @@ function calculate() {
     }
 }
 
-
-
+window.onload = function() {
+    document.getElementById("7").addEventListener("click", function() { 
+        addNumber('7'); });
+    document.getElementById("8").addEventListener("click", function() { 
+        addNumber('8'); });
+    document.getElementById("9").addEventListener("click", function() { 
+        addNumber('9'); });
+    document.getElementById("4").addEventListener("click", function() { 
+        addNumber('4'); });
+    document.getElementById("5").addEventListener("click", function() { 
+        addNumber('5'); });
+    document.getElementById("6").addEventListener("click", function() { 
+        addNumber('6'); });
+    document.getElementById("1").addEventListener("click", function() { 
+        addNumber('1'); });
+    document.getElementById("2").addEventListener("click", function() { 
+        addNumber('2'); });
+    document.getElementById("3").addEventListener("click", function() { 
+        addNumber('3'); });
+    document.getElementById("0").addEventListener("click", function() { 
+        addNumber('0'); });
+    document.getElementById("add").addEventListener("click", function() { 
+        addOperator('+'); });
+    document.getElementById("subtract").addEventListener("click", function() { 
+        addOperator('-'); });
+    document.getElementById("multiply").addEventListener("click", function() { 
+        addOperator('*'); });
+    document.getElementById("divide").addEventListener("click", function() { 
+        addOperator('/'); });
+    document.getElementById("equal").addEventListener("click", calculate);
+    document.getElementById("clearButton").addEventListener("click", clearInput);
+}
