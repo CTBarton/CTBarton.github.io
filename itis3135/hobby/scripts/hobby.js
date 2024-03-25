3ad20ed24765b3cd39b84783d8ea20962a2e62a2
MintChocoTaco <156540599+CTBarton@users.noreply.github.com>
2024-03-24 20:57:31 -0400

    Many changes

--- itis3135/hobby/scripts/hobby.js
+++ itis3135/hobby/scripts/hobby.js
@@ -1,11 +1,10 @@
 function showSection(sectionId) {
-    // Hide all sections
+ 
     var sections = document.querySelectorAll('section');
     sections.forEach(function(section) {
         section.classList.remove('active');
     });
 
-    // Show the selected section
     var selectedSection = document.getElementById(sectionId);
     selectedSection.classList.add('active');
 }
